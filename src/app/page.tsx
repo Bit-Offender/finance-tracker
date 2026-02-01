17f682ad20ef2c07f25df2b94e26a69ba49dc8c9
Bit-Offender <surarihansarkar23@gmail.com>
2026-02-01 21:44:47 +0530

    Screen fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import LandingNavbar from "@/components/LandingNavbar";
 import CallToAction from "@/components/landing-page/callToAction";
 import Hero from "@/components/landing-page/hero";
@@ -9,7 +8,7 @@ import Footer from "@/components/footer";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-b from-[#101628] to-[#161F38] text-white">
+      <div className="min-h-screen w-100% bg-gradient-to-b from-[#101628] to-[#161F38] text-white">
         <LandingNavbar />
           <Hero/>
           <Content/>
